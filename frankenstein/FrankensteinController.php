d9a92943679d6409467320c25df0d024aac38632
pekka <pekka.siiriainen@activeark.com>
2017-01-25 14:32:10 +0200

    More refactoring, moving all WordPress related stuff to FrankensteinController.

--- frankenstein/FrankensteinController.php
+++ frankenstein/FrankensteinController.php
@@ -13,4 +13,54 @@ class FrankensteinController
         $this->api = $json_api;
     }
 
+    public function getPostsOfType($typeString)
+    {
+        return get_posts([
+            'post_type' => $typeString
+        ]);
+    }
+
+    public function getOwnFields($post)
+    {
+        $meta = get_post_meta($post->ID);
+        foreach ($meta as $key => $value)
+        {
+            $meta[$key] = $value[0];
+        }
+
+        return $meta;
+    }
+
+    public function getTaxonomySingle($post, $taxonomyKey, $nameOnly = true)
+    {
+        $term = get_the_terms($post, $taxonomyKey)[0];
+
+        return $nameOnly ? $term->name : $term;
+    }
+
+    public function getItemId($post)
+    {
+        return $post->ID;
+    }
+
+    public function getItemTitle($post)
+    {
+        return $post->post_title;
+    }
+
+    public function getPictures($imageAttachmentArray)
+    {
+        $images = [];
+        foreach ($imageAttachmentArray as $data)
+        {
+            $imageId = (int) $data['file'];
+            $images[] = [
+                'small' => wp_get_attachment_image_src($imageId, 'thumbnail')[0],
+                'medium' => wp_get_attachment_image_src($imageId, 'medium')[0],
+                'original' => wp_get_attachment_url($imageId)
+            ];
+        }
+
+        return $images;
+    }
 }
--- frankenstein/controllers/cars.php
+++ frankenstein/controllers/cars.php
@@ -12,29 +12,22 @@ class JSON_API_Cars_Controller extends FrankensteinController
     public function getWithTaxonomies()
     {
         $formatted = [];
-        $cars = get_posts([
-            'post_type' => 'cars'
-        ]);
+        $cars = $this->getPostsOfType('cars');
+
         foreach ($cars as $car)
         {
-            $meta = get_post_meta($car->ID);
-            foreach ($meta as $key => $value)
-            {
-                $meta[$key] = $value[0];
-            }
-            $imageGalleryObject = unserialize($meta['picture_gallery']);
-            $color = get_the_terms($car, 'color')[0]->name;
-            $aspiration = get_the_terms($car, 'aspiration')[0]->name;
+            $fields = $this->getOwnFields($car);
+            $id = $this->getItemId($car);
             $formatted[] = [
-                'id' => $car->ID,
-                'title' => $car->post_title,
-                'registrationDate' => $meta['registered_date'],
-                'description' => $meta['description'],
-                'specs' => $meta['technical_specs'],
-                'color' => $color,
-                'price' => $meta['price'],
-                'aspiration' => $aspiration,
-                'pictures' => $this->getPictures($imageGalleryObject)
+                'id' => $id,
+                'title' => $this->getItemTitle($car),
+                'registrationDate' => $fields['registered_date'],
+                'description' => $fields['description'],
+                'specs' => $fields['technical_specs'],
+                'color' => $this->getTaxonomySingle($car, 'color'),
+                'price' => $fields['price'],
+                'aspiration' => $this->getTaxonomySingle($car, 'aspiration'),
+                'pictures' => $this->getPictures(unserialize($fields['picture_gallery']))
             ];
         }
 
@@ -42,21 +35,5 @@ class JSON_API_Cars_Controller extends FrankensteinController
             'success' => true,
             'cars' => $formatted
         ];
-    }
-
-    public function getPictures($imageAttachmentArray)
-    {
-        $images = [];
-        foreach ($imageAttachmentArray as $data)
-        {
-            $imageId = (int) $data['file'];
-            $images[] = [
-                'small' => wp_get_attachment_image_src($imageId, 'thumbnail')[0],
-                'medium' => wp_get_attachment_image_src($imageId, 'medium')[0],
-                'original' => wp_get_attachment_url($imageId)
-            ];
-        }
-
-        return $images;
     }
 }
